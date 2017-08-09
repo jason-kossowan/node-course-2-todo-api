8fcb1efb2b22547719de27162d1a23c673935f70
Jason Kossowan <jason.kossowan@zedisolutions.com>
2017-08-09 15:43:36 -0600

    GET /todos/:id added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -26,6 +27,24 @@ app.get('/todos', (request, response) => {
         response.send({ todos });
     }, (error) => {
         response.status(400).send(error);
+    });
+});
+
+app.get('/todos/:id', (request, response) => {
+    var id = request.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return response.status(404).send({});
+    };
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return response.status(404).send({});
+        }
+
+        response.send({ todo });
+    }).catch((error) => {
+        response.status(400).send({});
     });
 });
 
